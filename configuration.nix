f5c280daa50db6ca08d57cbe862ff10498a7d6df
Jay Turner <jaynicholasturner@gmail.com>
2026-03-07 15:06:33 +0000

    Enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./modules/nixos/hardware/bluetooth.nix
     ];
 
   # Bootloader.
@@ -92,7 +93,6 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
     ];
   };
 

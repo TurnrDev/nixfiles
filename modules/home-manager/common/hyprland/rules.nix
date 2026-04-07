cec02ec4f8d8cc5c4b5245d301bfed152a0f4b4a
Jay Turner <jaynicholasturner@gmail.com>
2026-04-07 14:47:08 +0100

    Add final changes this lunchbraek

--- modules/home-manager/common/hyprland/rules.nix
+++ modules/home-manager/common/hyprland/rules.nix
@@ -1,14 +1,18 @@
 { ... }:
 {
   # Native Hyprland window and workspace rules live here.
-  # This is intentionally empty for now so you can add rules declaratively.
+  # Window/layer rules: https://wiki.hyprland.org/Configuring/Window-Rules/
+  # Workspace rules: https://wiki.hyprland.org/Configuring/Workspace-Rules/
   #
   # Examples:
   # wayland.windowManager.hyprland.settings = {
-  #   windowrulev2 = [
-  #     "float,class:^(pavucontrol)$"
-  #     "center,class:^(pavucontrol)$"
-  #     "workspace 3 silent,class:^(discord)$"
+  #   windowrule = [
+  #     {
+  #       name = "float-pavucontrol";
+  #       "match:class" = "^(pavucontrol)$";
+  #       float = "on";
+  #       center = "on";
+  #     }
   #   ];
   #
   #   workspace = [
@@ -16,4 +20,22 @@
   #     "2, monitor:HDMI-A-1"
   #   ];
   # };
+
+  # Quickshell floating rules
+  wayland.windowManager.hyprland.settings.windowrule = [
+    {
+      name = "float-system-monitor";
+      "match:initial_title" = "^(System Monitor)$";
+      "match:initial_class" = "^(org.quickshell)$";
+
+      float = "on";
+      size = "835 660";
+    }
+    {
+      name = "float-quickshell";
+      "match:initial_class" = "^(org.quickshell)$";
+
+      float = "on";
+    }
+  ];
 }

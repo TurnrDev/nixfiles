02631bd836b6816cfc11add7c666e9c872577de9
Jay Turner <jaynicholasturner@gmail.com>
2026-04-07 13:24:51 +0100

    Move DMS binds to repo

--- modules/home-manager/common/dms/dms.nix
+++ modules/home-manager/common/dms/dms.nix
@@ -48,12 +48,54 @@ in
   };
 
   wayland.windowManager.hyprland.settings = {
-      source = "~/.config/hypr/dms/*";
-      env = [
-        "DMS_DISABLE_MATUGEN,1"
-        "DMS_DANKBAR_LAYER,overlay"
-        "DMS_HIDE_TRAYIDS,discord,spotify"
-      ];
+    source = [
+      "~/.config/hypr/dms/colors.conf"
+      "~/.config/hypr/dms/cursor.conf"
+      "~/.config/hypr/dms/layout.conf"
+      "~/.config/hypr/dms/outputs.conf"
+      "~/.config/hypr/dms/windowrules.conf"
+    ];
+    env = [
+      "DMS_DISABLE_MATUGEN,1"
+      "DMS_DANKBAR_LAYER,overlay"
+      "DMS_HIDE_TRAYIDS,discord,spotify"
+    ];
+    bind = [
+      "SUPER, space, exec, dms ipc call spotlight toggle"
+      "SUPER, V, exec, dms ipc call clipboard toggle"
+      "SUPER, M, exec, dms ipc call processlist focusOrToggle"
+      "SUPER, comma, exec, dms ipc call settings focusOrToggle"
+      "SUPER, N, exec, dms ipc call notifications toggle"
+      "SUPER SHIFT, N, exec, dms ipc call notepad toggle"
+      "SUPER, Y, exec, dms ipc call dankdash wallpaper"
+      "SUPER, TAB, exec, dms ipc call hypr toggleOverview"
+      "SUPER, X, exec, dms ipc call powermenu toggle"
+      "SUPER SHIFT, Slash, exec, dms ipc call keybinds toggle hyprland"
+      "SUPER ALT, L, exec, dms ipc call lock lock"
+      "CTRL ALT, Delete, exec, dms ipc call processlist focusOrToggle"
+      "CTRL SHIFT, Escape, exec, dms ipc call processlist focusOrToggle"
+      "SUPER SHIFT, W, exec, dms ipc call window-rules toggle"
+      "CTRL SHIFT, R, exec, dms ipc call workspace-rename open"
+      ", Print, exec, dms screenshot"
+      "CTRL, Print, exec, dms screenshot full"
+      "ALT, Print, exec, dms screenshot window"
+    ];
+    bindel = [
+      ", XF86AudioRaiseVolume, exec, dms ipc call audio increment 3"
+      ", XF86AudioLowerVolume, exec, dms ipc call audio decrement 3"
+      "CTRL, XF86AudioRaiseVolume, exec, dms ipc call mpris increment 3"
+      "CTRL, XF86AudioLowerVolume, exec, dms ipc call mpris decrement 3"
+      ", XF86MonBrightnessUp, exec, dms ipc call brightness increment 5 \"\""
+      ", XF86MonBrightnessDown, exec, dms ipc call brightness decrement 5 \"\""
+    ];
+    bindl = [
+      ", XF86AudioMute, exec, dms ipc call audio mute"
+      ", XF86AudioMicMute, exec, dms ipc call audio micmute"
+      ", XF86AudioPause, exec, dms ipc call mpris playPause"
+      ", XF86AudioPlay, exec, dms ipc call mpris playPause"
+      ", XF86AudioPrev, exec, dms ipc call mpris previous"
+      ", XF86AudioNext, exec, dms ipc call mpris next"
+    ];
   };
 
 }
--- modules/home-manager/common/hyprland/hyprland.nix
+++ modules/home-manager/common/hyprland/hyprland.nix
@@ -1,5 +1,13 @@
 { configs, pkgs, inputs, ... }:
 
+let
+  zoomInCommand =
+    "exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '.float * 1.1')";
+  zoomOutCommand =
+    "exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '(.float / 1.1) | if . < 1 then 1 else . end')";
+  zoomResetCommand = "exec, hyprctl -q keyword cursor:zoom_factor 1";
+in
+
 {
   imports = [
     ../dms/dms.nix
@@ -16,37 +24,113 @@
         "[workspace 2 silent] uwsm app -- spotify"
       ];
       bindd = [
-        "$mainMod, W, Launch Firefox, exec, uwsm app -- firefox"
-        "$mainMod, C, Launch IDE, exec, uwsm app -- code"
         "$mainMod, Super_L, Launch Spotlight, exec, dms ipc call spotlight toggle"
         "$mainMod, Super_R, Launch Spotlight, exec, dms ipc call spotlight toggle"
-        "$mainMod, Q, Close, killactive"
-        ", XF86AudioRaiseVolume, Raise Volume, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%"
-        ", XF86AudioLowerVolume, Lower Volume, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%"
-        ", XF86AudioMute, Mute Volume, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle"
-        ", XF86MonBrightnessUp, Brightness Up, exec, brightnessctl s +5%"
-        ", XF86MonBrightnessDown, Brightness Down, exec, brightnessctl s 5%-"
-        ", XF86AudioPlay, Media Play, exec, playerctl play-pause"
-        ", XF86AudioNext, Media Next, exec, playerctl next"
-        ", XF86AudioPrev, Media Previous, exec, playerctl previous"
-        "$mainMod, mouse_down, Zoom In, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '.float * 1.1')"
-        "$mainMod, mouse_up, Zoom Out, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '(.float / 1.1) | if . < 1 then 1 else . end')"
-        "$altMod, mouse_up, Reset Zoom, exec, hyprctl -q keyword cursor:zoom_factor 1"
-        "$altMod, mouse_down, Reset Zoom, exec, hyprctl -q keyword cursor:zoom_factor 1"
-        "$altMod, minus, Reset Zoom, exec, hyprctl -q keyword cursor:zoom_factor 1"
-        "$mainMod, O, Execute a fit operation, submap, fit-switch"
-        "$mainMod, up, Move layout up, layoutmsg, move -col"
-        "$mainMod, down, Move layout down, layoutmsg, move +col"
-        "$mainMod, left, Move layout left, layoutmsg, move -col"
-        "$mainMod, right, Move layout right, layoutmsg, move +col"
-        "$altMod, up, Move window left, layoutmsg, swapcol l"
-        "$altMod, down, Move window right, layoutmsg, swapcol r" 
-        "$altMod, left, Move window left, layoutmsg, swapcol l"
-        "$altMod, right, Move window right, layoutmsg, swapcol r"
+        "$mainMod, W, Launch Firefox, exec, uwsm app -- firefox"
+        "$mainMod, C, Launch IDE, exec, uwsm app -- code"
+        "$mainMod, G, Launch Git client, exec, uwsm app -- gitkraken"
+        "$mainMod, T, Launch Terminal, exec, ghostty"
+        "SUPER SHIFT, E, Exit Hyprland, exit"
+        "$mainMod, Q, Close Window, killactive"
+        "$mainMod, F, Fullscreen, fullscreen, 1"
+        "SUPER SHIFT, F, Exit Fullscreen, fullscreen, 0"
+        "SUPER SHIFT, T, Toggle Floating, togglefloating"
+
+        "$mainMod, left, Focus Left, movefocus, l"
+        "$mainMod, down, Focus Down, movefocus, d"
+        "$mainMod, up, Focus Up, movefocus, u"
+        "$mainMod, right, Focus Right, movefocus, r"
+        "$mainMod, H, Focus Left, movefocus, l"
+        "$mainMod, J, Focus Down, movefocus, d"
+        "$mainMod, K, Focus Up, movefocus, u"
+        "$mainMod, L, Focus Right, movefocus, r"
+
+        "SUPER SHIFT, left, Move Window Left, movewindow, l"
+        "SUPER SHIFT, down, Move Window Down, movewindow, d"
+        "SUPER SHIFT, up, Move Window Up, movewindow, u"
+        "SUPER SHIFT, right, Move Window Right, movewindow, r"
+        "SUPER SHIFT, H, Move Window Left, movewindow, l"
+        "SUPER SHIFT, J, Move Window Down, movewindow, d"
+        "SUPER SHIFT, K, Move Window Up, movewindow, u"
+        "SUPER SHIFT, L, Move Window Right, movewindow, r"
+
+        "$mainMod, Home, Focus First Window, focuswindow, first"
+        "$mainMod, End, Focus Last Window, focuswindow, last"
+
+        "SUPER CTRL, left, Focus Monitor Left, focusmonitor, l"
+        "SUPER CTRL, right, Focus Monitor Right, focusmonitor, r"
+        "SUPER CTRL, H, Focus Monitor Left, focusmonitor, l"
+        "SUPER CTRL, J, Focus Monitor Down, focusmonitor, d"
+        "SUPER CTRL, K, Focus Monitor Up, focusmonitor, u"
+        "SUPER CTRL, L, Focus Monitor Right, focusmonitor, r"
+
+        "SUPER SHIFT CTRL, left, Move Window To Left Monitor, movewindow, mon:l"
+        "SUPER SHIFT CTRL, down, Move Window To Lower Monitor, movewindow, mon:d"
+        "SUPER SHIFT CTRL, up, Move Window To Upper Monitor, movewindow, mon:u"
+        "SUPER SHIFT CTRL, right, Move Window To Right Monitor, movewindow, mon:r"
+        "SUPER SHIFT CTRL, H, Move Window To Left Monitor, movewindow, mon:l"
+        "SUPER SHIFT CTRL, J, Move Window To Lower Monitor, movewindow, mon:d"
+        "SUPER SHIFT CTRL, K, Move Window To Upper Monitor, movewindow, mon:u"
+        "SUPER SHIFT CTRL, L, Move Window To Right Monitor, movewindow, mon:r"
+
+        "$mainMod, Page_Down, Next Workspace, workspace, e+1"
+        "$mainMod, Page_Up, Previous Workspace, workspace, e-1"
+        "$mainMod, U, Next Workspace, workspace, e+1"
+        "$mainMod, I, Previous Workspace, workspace, e-1"
+        "SUPER CTRL, down, Send Window To Next Workspace, movetoworkspace, e+1"
+        "SUPER CTRL, up, Send Window To Previous Workspace, movetoworkspace, e-1"
+        "SUPER CTRL, U, Send Window To Next Workspace, movetoworkspace, e+1"
+        "SUPER CTRL, I, Send Window To Previous Workspace, movetoworkspace, e-1"
+
+        "SUPER SHIFT, Page_Down, Move Window To Next Workspace, movetoworkspace, e+1"
+        "SUPER SHIFT, Page_Up, Move Window To Previous Workspace, movetoworkspace, e-1"
+        "SUPER SHIFT, U, Move Window To Next Workspace, movetoworkspace, e+1"
+        "SUPER SHIFT, I, Move Window To Previous Workspace, movetoworkspace, e-1"
+
+        "$mainMod, mouse_down, Next Workspace, workspace, e+1"
+        "$mainMod, mouse_up, Previous Workspace, workspace, e-1"
+        "SUPER CTRL, mouse_down, Send Window To Next Workspace, movetoworkspace, e+1"
+        "SUPER CTRL, mouse_up, Send Window To Previous Workspace, movetoworkspace, e-1"
+
+        "$mainMod, 1, Workspace 1, workspace, 1"
+        "$mainMod, 2, Workspace 2, workspace, 2"
+        "$mainMod, 3, Workspace 3, workspace, 3"
+        "$mainMod, 4, Workspace 4, workspace, 4"
+        "$mainMod, 5, Workspace 5, workspace, 5"
+        "$mainMod, 6, Workspace 6, workspace, 6"
+        "$mainMod, 7, Workspace 7, workspace, 7"
+        "$mainMod, 8, Workspace 8, workspace, 8"
+        "$mainMod, 9, Workspace 9, workspace, 9"
+
+        "SUPER SHIFT, 1, Move Window To Workspace 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, Move Window To Workspace 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, Move Window To Workspace 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, Move Window To Workspace 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5, Move Window To Workspace 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6, Move Window To Workspace 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7, Move Window To Workspace 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8, Move Window To Workspace 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, Move Window To Workspace 9, movetoworkspace, 9"
+
+        "$mainMod, bracketleft, Preselect Left Column, layoutmsg, preselect l"
+        "$mainMod, bracketright, Preselect Right Column, layoutmsg, preselect r"
+        "$mainMod, R, Toggle Split, layoutmsg, togglesplit"
+        "SUPER CTRL, F, Reset Window Size, resizeactive, exact 100%"
+        "$mainMod, code:20, Expand Window Left, resizeactive, -100 0"
+        "$mainMod, code:21, Shrink Window Left, resizeactive, 100 0"
+        "SUPER SHIFT, P, Toggle DPMS, dpms, toggle"
+
+        "$altMod, mouse_down, Zoom In, ${zoomInCommand}"
+        "$altMod, mouse_up, Zoom Out, ${zoomOutCommand}"
+        "$altMod, 0, Reset Zoom, ${zoomResetCommand}"
       ];
       binde = [
-        "$mainMod, equal, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '.float * 1.1')"
-        "$mainMod, minus, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '(.float * 0.9) | if . < 1 then 1 else . end')"
+        "$mainMod, minus, resizeactive, -10% 0"
+        "$mainMod, equal, resizeactive, 10% 0"
+        "SUPER SHIFT, minus, resizeactive, 0 -10%"
+        "SUPER SHIFT, equal, resizeactive, 0 10%"
+        "$altMod, equal, ${zoomInCommand}"
+        "$altMod, minus, ${zoomOutCommand}"
       ];
       bindm = [
         "$mainMod, mouse:272, movewindow"

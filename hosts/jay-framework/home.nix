12f46d5fe4825546c20d530397be5f19d30add6f
Jay Turner <jaynicholasturner@gmail.com>
2026-04-26 13:00:20 +0100

    Use framework logo

--- hosts/jay-framework/home.nix
+++ hosts/jay-framework/home.nix
@@ -1,7 +1,14 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 {
   imports = [
     ../../modules/home-manager/roles/laptop.nix
   ];
+
+  programs.dank-material-shell.settings = {
+    dockLauncherLogoCustomPath = lib.mkForce "/etc/nixos/framework-logo.svg";
+    dockLauncherLogoMode = lib.mkForce "custom";
+    launcherLogoCustomPath = lib.mkForce "/etc/nixos/framework-logo.svg";
+    launcherLogoMode = lib.mkForce "custom";
+  };
 }

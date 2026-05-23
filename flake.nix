b2d1629470f77518f825fee27153b90b9c6bfdfb
Jay Turner <jaynicholasturner@gmail.com>
2026-05-23 20:54:47 +0100

    Add edmcoverlay2

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
       url = "git+https://github.com/elite-kode/edmc-discord-presence?submodules=1";
       flake = false;
     };
+    edmcoverlay2 = {
+      url = "git+https://github.com/pan-mroku/edmcoverlay2";
+      flake = false;
+    };
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/home-manager/roles/gaming.nix
+++ modules/home-manager/roles/gaming.nix
@@ -15,6 +15,56 @@ let
       runHook postInstall
     '';
   };
+
+  edmcoverlay2 = pkgs.stdenv.mkDerivation {
+    pname = "edmcoverlay2";
+    version = "unstable";
+    src = inputs.edmcoverlay2;
+
+    nativeBuildInputs = [
+      pkgs.makeWrapper
+    ];
+
+    buildInputs = [
+      pkgs.gtk4
+      pkgs.gtk4-layer-shell
+      pkgs.python3Packages.pygobject3
+      pkgs.libx11
+      pkgs.libxcomposite
+      pkgs.libxext
+      pkgs.libxfixes
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+      make
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p "$out/edmcoverlay"
+      cp -r . "$out/edmcoverlay"
+
+      chmod +x "$out/edmcoverlay/overlay"
+
+      mv "$out/edmcoverlay/Wayland/main.py" "$out/edmcoverlay/Wayland/main.py.real"
+      makeWrapper ${pkgs.python3.withPackages (ps: [ ps.pygobject3 ])}/bin/python "$out/edmcoverlay/Wayland/main.py" \
+        --add-flags "$out/edmcoverlay/Wayland/main.py.real" \
+        --prefix GI_TYPELIB_PATH : "${pkgs.lib.makeSearchPath "lib/girepository-1.0" [
+          pkgs.gtk4
+          pkgs.gtk4-layer-shell
+        ]}" \
+        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+          pkgs.gtk4
+          pkgs.gtk4-layer-shell
+        ]}" \
+        --prefix XDG_DATA_DIRS : "${pkgs.gtk4}/share:${pkgs.gtk4-layer-shell}/share"
+
+      runHook postInstall
+    '';
+  };
 in
 {
   home.packages = [
@@ -23,4 +73,7 @@ in
 
   home.file.".local/share/EDMarketConnector/plugins/DiscordPresence".source =
     "${edmcDiscordPresence}/DiscordPresence";
+
+  home.file.".local/share/EDMarketConnector/plugins/edmcoverlay".source =
+    "${edmcoverlay2}/edmcoverlay";
 }

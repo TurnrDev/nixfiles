76956e8a203a4321b225d2943568c525648055a2
Jay Turner <jaynicholasturner@gmail.com>
2026-05-24 01:06:06 +0100

    Remove theme

--- hosts/jay-framework/home.nix
+++ hosts/jay-framework/home.nix
@@ -10,11 +10,4 @@
   imports = [
     ../../modules/home-manager/roles/laptop.nix
   ];
-
-  programs.dank-material-shell.settings = {
-    dockLauncherLogoCustomPath = lib.mkForce "/etc/nixos/framework-logo.svg";
-    dockLauncherLogoMode = lib.mkForce "custom";
-    launcherLogoCustomPath = lib.mkForce "/etc/nixos/framework-logo.svg";
-    launcherLogoMode = lib.mkForce "custom";
-  };
 }

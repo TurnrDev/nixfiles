289b995ac9213f9b306d17f0753f6698a10ea2a8
Jay Turner <jaynicholasturner@gmail.com>
2026-06-06 20:16:06 +0100

    Add udev

--- modules/nixos/hardware/keychron.nix
+++ modules/nixos/hardware/keychron.nix
@@ -4,6 +4,7 @@
   # Allow the Keychron web configurator to access supported keyboards.
   services.udev.extraRules = ''
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0961", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d031", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
   '';
 
   services.xserver.inputClassSections = [

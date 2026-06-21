6f05d24fe8474d8f176ecddc0661ed1103649f5c
Jay Turner <jaynicholasturner@gmail.com>
2026-06-21 13:14:06 +0100

    Fix uv

--- modules/home-manager/common/hyprland/env.nix
+++ modules/home-manager/common/hyprland/env.nix
@@ -2,10 +2,7 @@
 {
   # Hyprland env lines rendered as `env=NAME,value`.
   # Keep one env assignment per string.
-  #
-  # Example:
-  # wayland.windowManager.hyprland.settings.env = [
-  #   "XCURSOR_SIZE,24"
-  #   "QT_QPA_PLATFORM,wayland;xcb"
-  # ];
+  wayland.windowManager.hyprland.settings.env = [
+    "UV_PROJECT_ENVIRONMENT,.venv"
+  ];
 }

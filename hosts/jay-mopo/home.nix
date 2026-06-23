a729dba701e0ecb0244c3372b0d0ce789e66fc1c
Jay Turner <jay.turner@mopo.co>
2026-06-23 10:42:51 +0100

    Add theme

--- hosts/jay-mopo/home.nix
+++ hosts/jay-mopo/home.nix
@@ -11,6 +11,9 @@
     ../../modules/home-manager/roles/laptop.nix
   ];
 
+  programs.dank-material-shell.settings.customThemeFile = lib.mkForce
+    "/etc/nixos/modules/home-manager/common/dms/themes/mopo.json";
+
   wayland.windowManager.hyprland.settings = {
     input.kb_variant = "colemak";
     "exec-once" = lib.mkForce [

83121741dc86535c9606ae78d19e9a8459a50ada
Jay Turner <jaynicholasturner@gmail.com>
2026-06-28 18:57:16 +0100

    Embed init.lua

--- modules/home-manager/common/hyprland/hyprland.nix
+++ modules/home-manager/common/hyprland/hyprland.nix
@@ -29,6 +29,24 @@ in
     configType = "lua";
     settings = { };
 
+    extraConfig = ''
+      require("generated")
+      require("config.core")
+
+      -- DMS owns these writable visual fragments. Monitor configuration remains
+      -- Nix-backed by config.core, with DMS outputs taking runtime precedence.
+      require("dms.colors")
+      require("dms.layout")
+      require("dms.outputs")
+      require("dms.cursor")
+
+      require("config.rules")
+      require("config.binds")
+
+      -- User-managed rules from DMS take final precedence.
+      require("dms.windowrules")
+    '';
+
     extraLuaFiles = {
       generated = {
         content = ''
@@ -37,7 +55,6 @@ in
         autoLoad = false;
       };
 
-      "config.init" = ./lua/init.lua;
       "config.core" = {
         content = ./lua/core.lua;
         autoLoad = false;

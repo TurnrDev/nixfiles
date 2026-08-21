fddb6c7567e90d0b95a14b26694d4ac5afce5123
Jay Turner <jay@turnr.dev>
2026-08-21 14:29:10 +0100

    udev

--- modules/nixos/hardware/keychron.nix
+++ modules/nixos/hardware/keychron.nix
@@ -1,11 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  # Allow the Keychron web configurator to access supported keyboards.
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0961", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d031", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-  '';
+  services.udev = {
+    packages = with pkgs; [
+      qmk
+      qmk-udev-rules # the only relevant
+      qmk_hid
+      via
+      vial
+    ]; # packages
+
+  }; # udev
+
+  # # Allow the Keychron web configurator to access supported keyboards.
+  # services.udev.extraRules = ''
+  #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0961", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d031", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  # '';
 
   services.xserver.inputClassSections = [
     ''
